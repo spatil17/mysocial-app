512e879e3ac7afefbac56e7b5abd196f2b8f21bb
spatil17 <31999492+spatil17@users.noreply.github.com>
2017-11-27 23:32:29 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,6 +3,6 @@ const crypto = require('crypto').randomBytes(256).toString('hex');
 module.exports = {
   //uri : 'mongodb://localhost:27017/' + this.db,
   uri : 'mongodb://<patil>:<shweta123>@ds121686.mlab.com:21686/angular-2-app'
-  secret : crypto,
+  secret : 'crypto',
     db: 'angular-2-ap'
 }
